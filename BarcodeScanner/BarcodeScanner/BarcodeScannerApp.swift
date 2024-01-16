d33fbe918f589a2a372860eeba243540fa49986d
Matt <matt.j.zbik@gmail.com>
2024-01-16 14:57:29 -0700

    feat: Initialized barcode scanner project.

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Matthew Zbik on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
