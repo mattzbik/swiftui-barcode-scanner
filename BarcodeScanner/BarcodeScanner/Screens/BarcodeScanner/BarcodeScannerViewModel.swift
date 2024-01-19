19a30e0b3d01b237eb4488df87d350c1fc95a615
Matt <matt.j.zbik@gmail.com>
2024-01-19 11:53:12 -0700

    feat: Error Handling, controller setup, and refactoring

---
+++ BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarcodeScanner
+//
+//  Created by Matthew Zbik on 2024-01-19.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
