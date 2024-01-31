43ac20afaa5a8da787d3ca38d6b2878279e64f87
Matt <matt.j.zbik@gmail.com>
2024-01-31 12:19:47 -0700

    fix: App optimizations for Barcode Scanner App ## Changes - Changed the struct properties of `title` and `message` to use `Text` over `String`. - Removed `maxWidth: .infinity` as by default it stretches as far as it can. - Updated `Alert` properties in `BarcodeScannerView` accordingly.

--- BarcodeScanner/BarcodeScanner/Utilities/Alert.swift
+++ BarcodeScanner/BarcodeScanner/Utilities/Alert.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
-                                               message: "Something is wrong with the camera. We are unable to capture the input.",
+    static let invalidDeviceInput = AlertItem(title: Text("Invalid Device Input"),
+                                               message: Text("Something is wrong with the camera. We are unable to capture the input."),
                                                dismissButton: .default(Text("OK")))
-    static let invalidScannedType = AlertItem(title: "Invalid Scan Type",
-                                               message: "The value scanned is not valid. This app scans EAN-8 and EAN-13 and QR.",
+    static let invalidScannedType = AlertItem(title: Text("Invalid Scan Type"),
+                                               message: Text("The value scanned is not valid. This app scans EAN-8 and EAN-13 and QR."),
                                                dismissButton: .default(Text("OK")))
 }
